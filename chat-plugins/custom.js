8f64e54c82afc0f04780158404c583957b4170ef
TrickTales <wolloyo@gmail.com>
2017-09-01 18:42:01 -0400

    Update custom.js

--- chat-plugins/custom.js
+++ chat-plugins/custom.js
@@ -1,9 +1,9 @@
 'use strict';
 
 exports.commands = {
-	serverrules: 'meadowrules',
-	meadowrules: function (target, room, user) {
-		this.popupReply("|html|" + "<font size=4><b>Meadow Server Rules:</b></font><br />" +
+	serverrules: 'sparkrules',
+	sparkrules: function (target, room, user) {
+		this.popupReply("|html|" + "<font size=4><b>Spark Server Rules:</b></font><br />" +
 					"<br />" +
 					"<b>1.</b> No sex. Don't discuss anything sexually explicit, not even in private messages, not even if you're both adults.<br />" +
 					"<br />" +
@@ -50,13 +50,4 @@ exports.commands = {
 	 	if (!this.runBroadcast()) return;
 	 	return this.sendReply('|raw|<center><img src="https://i.imgur.com/cVhyNfL.gif" width="300" height="169"></center>');
 	},
-
-	fleur : 'fleur',
-    	fleur: function(target, room, user) {
-            if (!this.runBroadcast()) return;
-            this.sendReplyBox('<center><tbody><table height="100%" width="100%" padding:5px; style="background-image: url(&quot;http://i.imgur.com/fpc8bl1.jpg&quot;); background-size: cover" table border="0"><tbody><tr><td width="25%" height="100%"><center><img height="180" src="https://s29.postimg.org/u6brormg7/yozora_g1.gif">' +
-                '<a href="http://picosong.com/rbeq/" target="_blank"><img src="https://s27.postimg.org/nd0zdnvur/fleur_text.png" height="125" alt="Theme" /></a>' +
-                '<img src="https://s29.postimg.org/9ngvjp8iv/yozora_g2.gif" height="180"><br />' +
-                '<b>Catchphrase: </b>"What kind of style do you desire to change into? Mesmerize your identity with fashion and make up. Blossom into an elegant flower, so that you may live your life with freedom and happiness."</center>');
-    	},
 };
