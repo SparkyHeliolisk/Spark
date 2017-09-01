d33e41bea6f8eefc8813694f9de68326b88af70c
AlfaStorm <wolloyo@gmail.com>
2017-09-01 17:30:32 +0000

    merge

--- chat-plugins/customcolor.js
+++ chat-plugins/customcolor.js
@@ -1,140 +1,115 @@
-/*
-- * Custom Color Plugins & hashcolor
-- * 
-*/
-
+/**
+ * Hashcolor file.
+ *
+ * Credits go to:
+ * - panpawn and jd
+ */
 'use strict';
 
-let filepath = DATA_DIR + 'customcolors.json'; 
+const fs = require('fs');
+
 let customColors = {};
-let fs = require('fs');
-let path = require('path');
 
-
-function load () {
-        fs.readFile(filepath, 'utf8', function (err, file) {
-                if (err) return;
-                customColors = JSON.parse(file);
-        });
+function load() {
+	fs.readFile('config/customcolors.json', 'utf8', function (err, file) {
+		if (err) return;
+		customColors = JSON.parse(file);
+	});
 }
-load();
-
-function logCustom(message) {
-       if (!message) return;
-       let file = path.join(LOGS_DIR + 'custom.txt');
-       let date = "[" + new Date().toUTCString() + "] ";
-       let msg = message + "\n";
-       fs.appendFile(file, date + msg);
-}
-
+setInterval(function () {
+	load();
+}, 500);
 
 function updateColor() {
-        fs.writeFileSync(filepath, JSON.stringify(customColors));
+	fs.writeFileSync('config/customcolors.json', JSON.stringify(customColors));
 
-        let newCss = '\n/*. New Custom-Colors (Delete Previous) .*/';
-        
-        for (let name in customColors) {
-                newCss += generateCSS(name, customColors[name]);
-        }
-        newCss += '';
-        
-        let file = fs.readFileSync(DATA_DIR + 'custom.css', 'utf8').split('');
-        if (~file.indexOf('')) file.splice(file.indexOf(''), (file.indexOf('') - file.indexOf('')) + 1);
-        fs.writeFileSync(DATA_DIR + 'custom.css', file.join('') + newCss);
+	let newCss = '/* COLORS START */\n';
+
+	for (let name in customColors) {
+		newCss += generateCSS(name, customColors[name]);
+	}
+	newCss += '/* COLORS END */\n';
+
+	let file = fs.readFileSync('config/custom.css', 'utf8').split('\n');
+	if (~file.indexOf('/* COLORS START */')) file.splice(file.indexOf('/* COLORS START */'), (file.indexOf('/* COLORS END */') - file.indexOf('/* COLORS START */')) + 1);
+	fs.writeFileSync('config/custom.css', file.join('\n') + newCss);
+	SG.reloadCSS();
 }
-global.updateColor = updateColor;
 
 function generateCSS(name, color) {
-        let css = '';
-        let rooms = [];
-        name = toId(name);
-        
-        Rooms.rooms.forEach((curRoom, id) => {
+	let css = '';
+	let rooms = [];
+	name = toId(name);
+	Rooms.rooms.forEach((curRoom, id) => {
 		if (id === 'global' || curRoom.type !== 'chat' || curRoom.isPersonal) return;
 		if (!isNaN(Number(id.charAt(0)))) return;
 		rooms.push('#' + id + '-userlist-user-' + name + ' strong em');
 		rooms.push('#' + id + '-userlist-user-' + name + ' strong');
 		rooms.push('#' + id + '-userlist-user-' + name + ' span');
-	    }  );
-        css = rooms.join(', ');
-        css += '{color: ' + color + ' !important;}';
-        css += '.chat.chatmessage-' + name + ' strong {';
-        css += 'color: ' + color + ' !important;}';
-        return css;
+	});
+	css = rooms.join(', ');
+	css += '{\ncolor: ' + color + ' !important;\n}\n';
+	css += '.chat.chatmessage-' + name + ' strong {\n';
+	css += 'color: ' + color + ' !important;\n}\n';
+	return css;
 }
 
 exports.commands = {
-    '!hex': true,
- 	hex: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		if (!this.canTalk()) return;
-		if (!target) target = toId(user.name);
-		return this.sendReplyBox('<b><font style="color:' + EM.Color(toId(target)) + '">' + target + '</font></b>.  Your hexadecimal color is: ' + EM.Color(target) + '.');
-	},
-    	customcolor: {
-		 '': function (target, room, user) {
-      if (!this.can('ban')) return false;
-			  this.sendReplyBox(
-				  '<small><em>(Only Administrators(~), Leaders(&) and Moderators(@) can use these commands)</em></small><br />' +
-				  '/customcolor set <em>‹user›</em>, <em>‹hex›</em><br />' +
-				  '/customcolor delete <em>‹user›</em><br />' +
-				  '/customcolor preview <em>‹user›</em>, <em>‹hex›</em><br />' +
-				  '/customcolor move <em>‹user1›</em>,<em>‹user2›</em>, <em>‹hex›</em><br />' +
-				  '/customcolor list<br>' +
-				  '<hr /><b>Note:</b> It is recommended to use a hexadecimal color (<a href="html-color-codes.info" target="_blank">Hex</a>).' 
-				);
-		},
-				   move: function(target, room, user, connection, cmd) {
-            if (!this.can('ban')) return false;
-    		if (!target || !target.trim()) return this.sendReply('/custom color move User 1, User 2, hex.');
-    		target = target.split(',');
-    		if (target.length < 3) return this.sendReply('/custom color move User 1, User 2, hex.');
-            
-            let user1 = (Users.getExact(target[0]) ? Users.getExact(target[0]).name : target[0]);
-    		let user2 = (Users.getExact(target[1]) ? Users.getExact(target[1]).name : target[1]);
-    		if (!toId(user1) || !toId(user2)) return this.sendReply('/custom color move User 1, User 2, hex.');
-    Rooms.rooms.get('staff').add('|raw|<small>[CUSTOMCOLOR] <b><font style="color:' + EM.Color(toId(user.name)) + '">' + user.name + '</font></b> has moved the customcolor of ' + target[0] + ' to '  + target[1] + ' ,color --> <font color="' + target[2] + '"><b>' + target[2] + '</b></font></small>');
-  	Rooms.rooms.get('staff').update();
-  		delete customColors[toId(target[0])];
-    	 customColors[toId(target[1])] = target[2];
-       updateColor();
-				   },
+	customcolour: 'customcolor',
+	customcolor: {
 		set: function (target, room, user) {
-		        if (!this.can('ban')) return false;
-                        target = target.split(',');
-                        
-                        this.sendReply("|raw|The color of <b><font color=" + target[1] + ">" + Chat.escapeHTML(target[0]) + "</font></b> has been successfully established.");
-                        logCustom(user.name + " has set a customcolor for " + target[0] + ". (Color: " + target[1] + ").");
-                        Rooms.rooms.get('staff').add('|raw|<small>[CUSTOMCOLOR] <b><font style="color:' + EM.Color(toId(user.name)) + '">' + user.name + '</font></b> has set a customcolor for ' + target[0] + ' --> <font color="' + target[1] + '"><b>' + target[1] + '</b></font></small>');
-			Rooms.rooms.get('staff').update();
-                        customColors[toId(target[0])] = target[1];
-                        updateColor();
+			if (!this.can('roomowner')) return false;
+			target = target.split(',');
+			for (let u = 0; u < target.length; u++) target[u] = target[u].trim();
+			if (!target[1]) return this.parse('/help customcolor');
+			if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
+			this.sendReply("|raw|You have given <b><font color=" + target[1] + ">" + Chat.escapeHTML(target[0]) + "</font></b> a custom color.");
+			this.privateModCommand("(" + target[0] + " has received custom color: '" + target[1] + "' from " + user.name + ".)");
+			Monitor.adminlog(target[0] + " has received custom color: '" + target[1] + "' from " + user.name + ".");
+			customColors[toId(target[0])] = target[1];
+			updateColor();
 		},
-		
 		delete: function (target, room, user) {
-		        if (!this.can('ban')) return false;
-		        if (!customColors[toId(target)]) return this.errorReply('The user:  ' + target + ' did not have a customcolor set.');
-                        delete customColors[toId(target)];
-                        updateColor();
-                        logCustom(user.name + " has removed the customcolor of " + target[0] + ".");
-                        this.sendReply("You have removed the customcolor of " + target + ".");
-                        if (Users(target) && Users(target).connected) Users(target).popup(user.name + " has removed your customcolor.");
+			if (!this.can('roomowner')) return false;
+			if (!target) return this.parse('/help customcolor');
+			if (!customColors[toId(target)]) return this.errorReply('/customcolor - ' + target + ' does not have a custom color.');
+			delete customColors[toId(target)];
+			updateColor();
+			this.sendReply("You removed " + target + "'s custom color.");
+			this.privateModCommand("(" + target + "'s custom color was removed by " + user.name + ".)");
+			Monitor.adminlog(target + "'s custom color was removed by " + user.name + ".");
+			if (Users(target) && Users(target).connected) Users(target).popup(user.name + " removed your custom color.");
+			return;
 		},
-		
 		preview: function (target, room, user) {
-		        if (!this.runBroadcast()) return;
-                        target = target.split(',');
-                        for (let u in target) target[u] = target[u].trim();
-                        if (!target[1]) return this.parse('/help colorpreview');
-                        //return this.sendReplyBox('Asi se veria el nick de <b><font size="2" color="' +  target[1] + '">' + Chat.escapeHTML(target[0]) + '</font></b> con un color personalizado.');
-                        this.sendReply('|raw|<b><font size="2" color="' +  target[1] + '">' + Chat.escapeHTML(target[0]) + ':</font></b> Look what my nick looks like with the color <b><font size="2" color="' +  target[1] + '">' + target[1] + '</font></b>');
+			if (!this.runBroadcast()) return;
+			target = target.split(',');
+			for (let u = 0; u < target.length; u++) target[u] = target[u].trim();
+			if (!target[1]) return this.parse('/help customcolor');
+			return this.sendReplyBox('<b><font size="3" color="' + target[1] + '">' + Chat.escapeHTML(target[0]) + '</font></b>');
 		},
-    list: function (target, room, user) {
-		if (!this.can('ban')) return false;
-		let colores = fs.readFileSync( DATA_DIR + 'customcolors.json', 'utf8').split('\n').splice(-100).join('\n');
-		user.send('|popup||html|' + '<center>List of users with a customcolor and their colors</center><br>' + colores);
+		reload: function (target, room, user) {
+			if (!this.can('hotpatch')) return false;
+			updateColor();
+			this.privateModCommand("(" + user.name + " has reloaded custom colours.)");
+		},
+		'': function (target, room, user) {
+			return this.parse("/help customcolor");
+		},
 	},
-		}
+	customcolorhelp: [
+		"Commands Include:",
+		"/customcolor set [user], [hex] - Gives [user] a custom color of [hex]",
+		"/customcolor delete [user], delete - Deletes a user's custom color",
+		"/customcolor reload - Reloads colours.",
+		"/customcolor preview [user], [hex] - Previews what that username looks like with [hex] as the color.",
+	],
+	'!hex': true,
+	hex: function (target, room, user) {
+		if (!this.runBroadcast()) return;
+		let targetUser = (target ? target : user.name);
+		this.sendReplyBox('The hex code of ' + EM.nameColor(targetUser, true) + ' is: <font color="' + EM.Color(targetUser) + '"><b>' + SG.hashColor(targetUser) + '</b></font>');
+	},
 };
 /*
 - * hashColor
