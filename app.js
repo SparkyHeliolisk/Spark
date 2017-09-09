b77466fe434d5726c8509cf372a32280f01b8c74
TrickTales <wolloyo@gmail.com>
2017-09-09 18:31:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -108,6 +108,8 @@ global.Db = require('origindb')('config/db');
 
 global.EM = {};
 
+global.sqlite3 = require('sqlite3');
+
 global.Rooms = require('./rooms');
 
 global.Verifier = require('./verifier');
